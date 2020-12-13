7fc6af949bcbe74aec2587ea571ae363bbae4436
kelly_manahan <kelly.a.manahan@gmail.com>
2020-12-13 11:16:01 -0700

    dotenv in config file

---
+++ app/models/search.rb
@@ -0,0 +1,17 @@
+class Search 
+    attr_reader :results 
+
+    
+    def initialize(location)
+        url = "https://api.yelp.com/v3/businesses/search"
+        params = {
+          term: "lunch",
+          location: location,
+          limit: 50
+        }
+    
+        response = HTTP.auth("Bearer #{ENV['YELP_KEY']}").get(url, params: params)
+        @results = response.parse
+      end
+
+end 
